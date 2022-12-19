e35136edb0a054e46cdde82bd1869c2c144d70e7
Evgen65 <uevgeny1965@gmail.com>
2022-12-19 21:54:51 +0200

    upDate19_12

--- src/main/java/manager/HelperCar.java
+++ src/main/java/manager/HelperCar.java
@@ -1,14 +1,11 @@
 package manager;
 
 import models.Car;
-import models.User;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.xml.sax.Locator;
 
 public class HelperCar extends HelperBase {
     public HelperCar(WebDriver wd) {
